f919b9b1eace6b750dbdc0bd82c5287681cc2493
Justin Lubin <jlubi333@gmail.com>
2014-07-20 21:41:29 -0400

    Changed logout to log out in nav.

--- app/views/layout/nav.blade.php
+++ app/views/layout/nav.blade.php
@@ -4,7 +4,7 @@
         @if(Auth::check())
             <li>{{ link_to_route('game.lobby', 'Lobby') }}</li>
             <li>{{ link_to_route('users.profile', 'Profile', $parameters=['username' => Auth::user()->username])}}</li>
-            <li>{{ link_to_route('users.getLogOut', 'Logout') }}</li>
+            <li>{{ link_to_route('users.getLogOut', 'Log Out') }}</li>
         @else
             <li>{{ link_to_route('users.getLogIn', 'Log In') }}</li>
             <li>{{ link_to_route('users.getSignUp', 'Sign Up') }}</li>
