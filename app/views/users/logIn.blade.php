6be36bf761c5b39542cee9ee3f1eecd761e0ec5e
Justin Lubin <jlubi333@gmail.com>
2014-07-20 22:43:00 -0400

    Fixed :message typo in login.

--- app/views/users/logIn.blade.php
+++ app/views/users/logIn.blade.php
@@ -10,12 +10,12 @@
         {{ Form::open(['route' => 'users.postLogIn']) }}
             <div>
                 {{ Form::label('username', 'Username') }}
-                {{ $errors->first('username', '<span class="error">:mesage</span>') }}
+                {{ $errors->first('username', '<span class="error">:message</span>') }}
                 {{ Form::text('username') }}
             </div>
             <div>
                 {{ Form::label('password', 'Password') }}
-                {{ $errors->first('password', '<span class="error">:mesage</span>') }}
+                {{ $errors->first('password', '<span class="error">:message</span>') }}
                 {{ Form::password('password') }}
             </div>
             {{ Form::submit('Log In') }}
