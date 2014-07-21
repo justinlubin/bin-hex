965e25242fa77161bb3c83e308e2ed4341b64274
Justin Lubin <jlubi333@gmail.com>
2014-07-21 16:52:44 -0400

    Reworded single player game lobby button.

--- app/views/game/lobby.blade.php
+++ app/views/game/lobby.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
     <h1>Lobby</h1>
-    <div id="singleplayer"><h3>{{ link_to_route('game.getSingleplayer', 'Single Player Game') }}</h3></div>
+    <div id="singleplayer"><h3>{{ link_to_route('game.getSingleplayer', 'Play a Single Player Game') }}</h3></div>
 
     {{ Form::open(['route' => 'game.postMultiplayer']) }}
         <h3>Multiplayer Game</h3>
