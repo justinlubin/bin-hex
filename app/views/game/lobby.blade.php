5790c13ac619f4a77a6c74e039033ded3b17df4b
Justin Lubin <jlubi333@gmail.com>
2014-07-31 15:28:52 -0400

    Changed lobby to game lobby.

--- app/views/game/lobby.blade.php
+++ app/views/game/lobby.blade.php
@@ -1,11 +1,11 @@
 @extends('layout.default')
 
 @section('title')
-    Lobby
+    Game Lobby
 @stop
 
 @section('content')
-    <h1>Lobby</h1>
+    <h1>Game Lobby</h1>
     <div id="singleplayer"><h3>{{ link_to_route('game.getSinglePlayer', 'Play a Single Player Game') }}</h3></div>
 
     {{ Form::open(['route' => 'game.postMultiplayer']) }}
--- app/views/layout/nav.blade.php
+++ app/views/layout/nav.blade.php
@@ -4,7 +4,7 @@
         <li>{{ link_to_route('users.all', 'All Users') }}</li>
         <li>{{ link_to_route('users.leaderboard', 'Leaderboard') }}</li>
         @if(Auth::check())
-            <li>{{ link_to_route('game.lobby', 'Lobby') }}</li>
+            <li>{{ link_to_route('game.lobby', 'Game Lobby') }}</li>
             <li>{{ link_to_route('users.profile', 'Profile', $parameters = ['username' => Auth::user()->username])}}</li>
             <li>{{ link_to_route('users.getLogOut', 'Log Out') }}</li>
         @else
